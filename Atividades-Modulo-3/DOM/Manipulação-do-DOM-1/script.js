81d51bb3a97b83b220fc663e0c0a2be2dfeaf4c2
Bernardo Dartora <bernardintd@gmail.com>
2023-11-30 16:15:23 -0300

    att-dom-1

--- Atividades-Modulo-3/DOM/Manipulação-do-DOM-1/script.js
+++ Atividades-Modulo-3/DOM/Manipulação-do-DOM-1/script.js
@@ -1,8 +1,34 @@
-const fraseLocal = document.querySelector('h1')
+
 
 function digaOla(){
+    const fraseLocal = document.querySelector('h1')
 
     const nomeUser = prompt("Digite o seu nome")
     const frase = `E aí ${nomeUser}; Você está deixando o seu sitedinâmico.`
+
     fraseLocal.innerText = frase
+
 }
+
+
+let contarAtual = 0
+
+    const divContador = document.getElementById("contadorAtivo")
+    const paragrafo = document.createElement('p')
+    paragrafo.innerText = `o contador esta em ${contarAtual} cliques` 
+    divContador.appendChild(paragrafo)
+
+
+function incrementar(){
+    contarAtual++
+    paragrafo.innerText = `o contador esta em ${contarAtual} cliques` 
+    
+}
+
+function zerar(){
+    contarAtual = 0
+    paragrafo.innerText = ``
+}
+
+    
+    
