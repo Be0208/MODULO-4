a5077a5ab5f8a855871c97c1a6ef7092da82542d
Bernardo Dartora <bernardintd@gmail.com>
2023-11-30 15:34:50 -0300

    dom

--- Aulas Modulo 4/DOM/aula/scripts.js
+++ Aulas Modulo 4/DOM/aula/scripts.js
@@ -11,17 +11,35 @@
 const circulo = document.querySelector('#circulo') //pegou o id circulo, pois sinalizou com o # = id
 
 function pintarDeVermelho(){ //função adicionada no onclic Botao
-    circulo.classList.remove('violet')
-    circulo.classList.add('red')
+    const circulo = document.querySelector('#circulo')
+    circulo.style.backgroundColor = 'red'
 }
 
 function pintarDeVioleta(){
-    circulo.classList.remove('red')
-    circulo.classList.add('violet')
+    const circulo = document.querySelector('#circulo')
+    circulo.style.backgroundColor = 'violet'
+}
+
+// _______________INNER HTML ________________
+
+
+function aparecerCirculo(){ //add elemento no html
+    document.body.innerHTML += `
+    <div id="circulo"></div>`  
+
+    const circulo = document.querySelector('#circulo')
+    circulo.setAttribute('class', 'circulo')
+    circulo.setAttribute('onclick', 'pintarViolet()')
 }
 
 
+let clique = 0
 
 
+function addClique(){
 
+    clique += 1
+    
+    document.querySelector('#contador').innerHTML = clique
 
+}
