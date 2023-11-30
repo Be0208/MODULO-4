e951902f247c9b0eb436e19f3e05494aee0606b9
Bernardo Dartora <bernardintd@gmail.com>
2023-11-30 16:56:20 -0300

    dom

--- Atividades-Modulo-3/DOM/Manipulação-do-DOM-2/script.js
+++ Atividades-Modulo-3/DOM/Manipulação-do-DOM-2/script.js
@@ -1,6 +1,30 @@
 // script.js
-const cards = document.querySelectorAll('.card');
+const cards = document.querySelectorAll('.card')
+const titulosCards = document.querySelectorAll('.titulo-card')
+const descricaoCards = document.querySelectorAll('.descricao-card')
+const botaoEditar = document.querySelectorAll('.botao-editar')
+const botaoApagar = document.querySelectorAll('.botao-apagar')
+
+
+
 
 cards.forEach(card => {
-    card.classList.add('estilo-customizado');
-});
+  card.setAttribute('id', 'estilo-customizado')
+})
+
+titulosCards.forEach(card => {
+  card.setAttribute('id', 'titulo-card')
+})
+
+descricaoCards.forEach(card => {
+  card.setAttribute('id', 'descricao-card')
+})
+
+botaoEditar.forEach(card => {
+  card.setAttribute('id', 'botao-editar')
+})
+
+botaoApagar.forEach(card => {
+  card.setAttribute('id', 'botao-apagar')
+})
+
